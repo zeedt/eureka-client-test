8c6f78dd50166dcb1a15bfbf8d83859105f4438f
zeedt <yusufsaheedtaiwo@gmail.com>
2020-10-07 08:00:20 +0100

    first commit

---
+++ src/main/java/com/zeed/spring/eureka/client/GreetingController.java
@@ -0,0 +1,8 @@
+package com.zeed.spring.eureka.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    @RequestMapping("/greeting")
+    String greeting();
+}
---
+++ src/test/java/com/zeed/spring/eureka/client/EurekaClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeed.spring.eureka.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
